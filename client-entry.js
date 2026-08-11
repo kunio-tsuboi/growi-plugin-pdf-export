005963d248d47b270430940def1856cb25c1b760
kunio-tsuboi <kunio_tsuboi@unou.co.jp>
2026-08-11 13:14:46 +0900

    test running version

--- client-entry.js
+++ client-entry.js
@@ -95,3 +95,4 @@ window.pluginActivators[config.name] = {
     activate,
     deactivate,
 };
+activate();
--- client-entry.tsx
+++ client-entry.tsx
@@ -161,3 +161,5 @@ const deactivate = () => {
     activate,
     deactivate,
 };
+
+activate();
